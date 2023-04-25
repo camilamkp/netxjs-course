4e894ddbedcf667ebd714e2b200647e72e45737d
camilamkp <navemoin@gmail.com>
2023-04-25 11:34:43 +0200

    :construction: Update [id].js

--- events-app/src/pages/events/[cat]/[id].js
+++ events-app/src/pages/events/[cat]/[id].js
@@ -1,12 +1,51 @@
-import React from 'react';
+import Image from 'next/image';
 
-const SingleEventPage = () =>
+const SingleEventPage = ({ data }) =>
 {
   return (
     <div>
-    <h1>Our Single Event</h1>
-</div>
+
+      <Image src={ data.image } width={ 1000 } height={ 500 } alt={ data.title } />
+      <h1>{ data.title }</h1>
+      <p>{ data.description }</p>
+    </div>
   )
 }
 
 export default SingleEventPage;
+
+export async function getStaticPaths()
+{
+  const data = await import('../../../data/data.json');
+  const allEvents = data.allEvents;
+
+  const allPaths = allEvents.map((path) =>
+  {
+    return{
+      params: {
+        cat: path.city,
+        // the same name from the file [id]
+        id: path.id,
+      },
+    };
+  });
+
+  return{
+          paths: allPaths,
+          fallback: false,
+        }
+}
+
+export async function getStaticProps(context)
+{
+  const id = context.params.id;
+  const { allEvents } = await import('../../../data/data.json');
+  
+  const eventData = allEvents.find((ev) => id === ev.id );
+  
+  return{
+    props: {
+      data: eventData,
+    },
+  }
+}
