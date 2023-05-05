4bf1c876874d72e5b7d9946ada9113f51501b792
camilamkp <navemoin@gmail.com>
2023-05-05 21:16:19 +0200

    error on fetch data

--- events-app/src/components/events/EventCard.jsx
+++ events-app/src/components/events/EventCard.jsx
@@ -1,9 +1,36 @@
+import React, { useRef } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 
 const EventCard = ({ data }) =>
 {
-  const onSubmit = () => {};
+  const inputEmail = useRef();
+  const router = useRouter();
   
+  const onSubmit = async(e) =>
+  {
+      e.preventDefault();
+      const emailValue = inputEmail.current.value;
+      const eventId = router?.query.id;
+
+      try {
+        const response = await fetch('@/pages/api/email-registration',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ email: emailValue, eventId }),
+        });
+        if(!response.ok) throw new Error(`Error: ${ response.status }`);
+        const data = await response.json();
+        console.log('POST', data);
+        
+      } catch (e) {
+        console.log('error', e);
+      }
+  };
+
   return (
     <div className='event-card-container'>
       <h1>{ data.title }</h1>
@@ -13,7 +40,7 @@ const EventCard = ({ data }) =>
 
       <form onSubmit={ onSubmit } className='email-registration'>
         <label>Get Registered for this event!</label>
-        <input type='email' id='email' placeholder='Please insert your email here'/> 
+        <input ref={ inputEmail } type='email' id='email' placeholder='Please insert your email here'/> 
         <button type='submit'>Submit</button>
       </form>
 
